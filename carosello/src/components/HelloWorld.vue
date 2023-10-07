d5839ccd43c85e59e3586b7ae70719a7fd134b55
Marioditrani <marioditrani2003@gmail.com>
2023-10-07 15:23:11 +0200

    first commit

---
+++ carosello/src/components/HelloWorld.vue
@@ -0,0 +1,46 @@
+<script>
+export default{
+  data:()=> ({
+    images :[
+    "https://picsum.photos/id/1032/900/400",
+    "https://picsum.photos/id/1033/900/400",
+    "https://picsum.photos/id/1034/900/400",
+    "https://picsum.photos/id/1035/900/400",
+    "https://picsum.photos/id/1036/900/400",
+    ]
+  })
+}
+
+
+</script>
+
+<template>
+ <div class="carosello">
+  <div class="carosello-container">
+    <div class="carosello-item" v-for="image in images" :key="image">
+      <img :src="image" />
+    </div>
+  </div>
+  <div class="button">
+    <div class="button-up">&#8593;</div>
+    <div class="button-down">&#8595;</div>
+  </div>
+ </div>
+</template>
+
+<style scoped>
+.carosello{
+  display: flex;
+  justify-content: center;
+  gap: 30px;
+}
+
+.button{
+  display: flex;
+  justify-content: space-between;
+  flex-direction: column;
+}
+.button-up, .button-down{
+  font-size: 50px;
+}
+</style>
